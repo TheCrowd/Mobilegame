78e357bcb072d776b8571af02566f5c7802a9c8c
shengluz <xihaianzhang@gmail.com>
2018-10-21 13:39:16 +1100

    skinManager

---
+++ Assets/Snaker/Service/SkinManager.cs
@@ -0,0 +1,23 @@
+﻿using Snaker.Service.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Snaker.Service
+{
+    class SkinManager : ServiceModule<SkinManager>
+    {
+        private int skinType = 0;
+        public int SkinType { get { return skinType; } }
+        public void Init()
+        {
+            CheckSingleton();
+        }
+
+        public void UpdateSkinType(int extSkinType)
+        {
+            skinType = extSkinType;
+        }
+    }
+}
---
+++ Assets/Snaker/UI/Home/UIChangeSkinWin.cs
@@ -0,0 +1,27 @@
+﻿using Snaker.Service;
+using Snaker.Service.UIManager;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine.UI;
+
+namespace Snaker.UI.Home
+{
+    class UIChangeSkinWin:UIWindow
+    {
+        public Button[] buttons;
+        protected override void OnOpen(object arg = null)
+        {
+            base.OnOpen(arg);
+            int activeButton = SkinManager.Instance.SkinType;
+            buttons[activeButton].Select();
+        }
+
+        public void OnBtnSkinClick(int skinType)
+        {
+            SkinManager.Instance.UpdateSkinType(skinType);
+        }
+
+    }
+}
